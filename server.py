15a9713ae98eb0889efd73b5a900418032b16ef6
MADDIPATLA PHANINDRA <phani06041@gmail.com>
2023-08-23 21:44:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,34 +1,26 @@
 from flask import Flask, request, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return 'Hello world!'
+    return "Hello world!", 200
 
 @app.route('/calculator/add', methods=['POST'])
 def add():
     data = request.json
-    first = data.get('first')
-    second = data.get('second')
-    
-    if first is None or second is None:
-        return jsonify({"error": "Both 'first' and 'second' must be provided"}), 400
-    
+    first = data.get('first', 0)
+    second = data.get('second', 0)
     result = first + second
-    return jsonify({"result": result})
+    return jsonify({'result': result}), 200
 
 @app.route('/calculator/subtract', methods=['POST'])
 def subtract():
     data = request.json
-    first = data.get('first')
-    second = data.get('second')
-    
-    if first is None or second is None:
-        return jsonify({"error": "Both 'first' and 'second' must be provided"}), 400
-    
+    first = data.get('first', 0)
+    second = data.get('second', 0)
     result = first - second
-    return jsonify({"result": result})
+    return jsonify({'result': result}), 200
 
-if _name_ == '_main_':
-    app.run(port=8080, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run()
